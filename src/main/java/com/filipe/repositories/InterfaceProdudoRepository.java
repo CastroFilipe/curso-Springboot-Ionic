c82911a50cff0ae53a8cfbd85dc903beb3639077
Filipe Castro <filipe.couto.10@gmail.com>
2019-05-28 18:06:24 -0300

    Classe Produto e associacao ManyToMany com a Classe Categoria

---
+++ src/main/java/com/filipe/repositories/InterfaceProdudoRepository.java
@@ -0,0 +1,12 @@
+package com.filipe.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.filipe.domain.Produto;
+
+@Repository
+public interface InterfaceProdudoRepository extends JpaRepository<Produto, Long> {
+	
+	
+}
